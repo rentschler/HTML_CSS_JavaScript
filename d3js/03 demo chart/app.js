4a44b372e9a53a2850c3536b9f6542368977999f
monschter <73845942+monschter@users.noreply.github.com>
2023-03-27 17:47:11 +0200

    added chart

--- d3js/03 demo chart/app.js
+++ d3js/03 demo chart/app.js
@@ -4,3 +4,73 @@ const DUMMY_DATA = [
     {id: 'd3', value: 12, region: 'China'},
     {id: 'd4', value: 6, region: 'Germany'},
 ];
+
+const MARGINS = {top: 20, bottom: 10};
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
+
+const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
+const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+
+const charContainer = d3.select('svg')
+    .attr('width', CHART_WIDTH)
+    .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom)
+
+x.domain(DUMMY_DATA.map(d => d.region));//specify which data should be scaled
+y.domain([0, d3.max(DUMMY_DATA, d => d.value) + 3]);//specify which data should be scaled
+
+//render new rectangles with that class bar for all the missing data-points
+const chart = charContainer.append('g');//append a svg element, use g to group elements
+chart.selectAll('.bar')//select all elements with class bar
+    .data(DUMMY_DATA)//bind data to the elements
+    .enter()//get access for the missing elements
+    .append('rect')//append a rect element
+    .classed('bar', true)//add class bar to the element
+    .attr('width', x.bandwidth())//bandwidth gives us an equal space between the bars
+    .attr('height', data => CHART_HEIGHT - y(data.value))
+    .attr('x', data => x(data.region))
+    .attr('y', data => y(data.value));
+
+//add labels above the bars
+chart.selectAll('.label').data(DUMMY_DATA).enter()
+    .append('text')//adds a text element
+    .text(data => data.value)//set the text to the value of the data
+    .attr('x', data => x(data.region) + x.bandwidth() / 2)//set the x position to the middle of the bar
+    .attr('y', data => y(data.value) - 20)//set the y position to the top of the bar
+    .attr('text-anchor', 'middle')//set the anchor to the middle of the text
+    .classed('label', true);//add class label to the element
+
+//label below the bar, x-axis
+chart.append('g').call(d3.axisBottom(x).tickSizeOuter(0))//axis bottom extracts the axis information from the scale
+    //axisBottom defines, that the labels should be below the axis
+    .attr('color', '#4f009e')
+    .attr('transform', `translate(0, ${CHART_HEIGHT})`);//move the axis to the bottom of the chart
+
+
+//add and remove elements
+
+//keep track of unchecked regions
+let unselectedIds = [];
+
+const listItems = d3.select('#data')
+    .select('ul')
+    .selectAll('li')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('li');
+listItems.append('span').text(data => data.region);
+
+listItems.append('input')
+    .attr('type', 'checkbox')
+    .attr('checked', true)//set the checkbox to checked
+    .on('change', (data) => {
+        if (unselectedIds.indexOf(data.id) === -1) {
+            //if the id is not in the array
+            unselectedIds.push(data.id);
+        } else {
+            unselectedIds = unselectedIds.filter(id => id !== data.id);
+            //only keep the ids that are not equal to the id of the data
+        }
+        // console.log(data.target);
+        // console.log(unselectedIds);
+    })//add a listener to the checkbox
