f0d476072f8bcdfce6412187056016b7f7008ab0
monschter <73845942+monschter@users.noreply.github.com>
2023-04-01 15:52:38 +0200

    Update app.js
    you can add more datapoints now

--- d3js/03 demo chart/app.js
+++ d3js/03 demo chart/app.js
@@ -1,74 +1,75 @@
 const DUMMY_DATA = [
     {id: 'd1', value: 10, region: 'USA'},
-    {id: 'd2', value: 11, region: 'India'},
+    {id: 'd2', value: 9, region: 'India'},
     {id: 'd3', value: 12, region: 'China'},
     {id: 'd4', value: 6, region: 'Germany'},
 ];
+const DUMMY_VALUE = DUMMY_DATA.map(data => data.value);
 
 const MARGINS = {top: 20, bottom: 10};
 const CHART_WIDTH = 600;
 const CHART_HEIGHT = 400 - MARGINS.top - MARGINS.bottom;
 
+let numOFBars = DUMMY_DATA.length;
+
 // let selectedData = DUMMY_DATA;
 // create a copy of the original array using the spread operator. Here's an example:
 
-// let selectedData = [...DUMMY_DATA];
-let selectedData = [
-    {id: 'd1', value: 10, region: 'USA'},
-    {id: 'd2', value: 11, region: 'India'},
-    {id: 'd3', value: 12, region: 'China'},
-    {id: 'd4', value: 6, region: 'Germany'},
-];
-const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
-const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+let selectedData = DUMMY_DATA.filter(()=>true)
 
 const charContainer = d3.select('svg')
     .attr('width', CHART_WIDTH)
     .attr('height', CHART_HEIGHT + MARGINS.top + MARGINS.bottom)
 
-x.domain(DUMMY_DATA.map(d => d.region));//specify which data should be scaled
-y.domain([0, d3.max(DUMMY_DATA, d => d.value) + 3]);//specify which data should be scaled
+/* ******************************************* */
+/* ************** SCALE ********************* */
+    const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
+    const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+function adjustScale(){
+    x.domain(selectedData.map(d => d.region));//specify which data should be scaled
+    y.domain([0, d3.max(selectedData, d => d.value) + 3]);//specify which data should be scaled
+}
+adjustScale();
+
+
 
 //render new rectangles with that class bar for all the missing data-points
 const chart = charContainer.append('g');//append a svg element, use g to group elements
 
-//label below the bar, x-axis
-chart.append('g').call(d3.axisBottom(x).tickSizeOuter(0))//axis bottom extracts the axis information from the scale
+/* ******************************************* */
+/* ************** AXES ********************* */
+const axe = chart.append('g');
+axe.call(d3.axisBottom(x).tickSizeOuter(0))//axis bottom extracts the axis information from the scale
     //axisBottom defines, that the labels should be below the axis
     .attr('color', '#4f009e')
     .attr('transform', `translate(0, ${CHART_HEIGHT})`);//move the axis to the bottom of the chart
 
+/* ******************************************* */
+/* ************** CHART ********************* */
 function renderChart(){
+    adjustScale();
+    axe.call(d3.axisBottom(x).tickSizeOuter(0))//axis bottom extracts the axis information from the scale
+        //axisBottom defines, that the labels should be below the axis
+        .attr('color', '#4f009e')
+        .attr('transform', `translate(0, ${CHART_HEIGHT})`);//move the axis to the bottom of the chart
+
+    console.log(toString(selectedData));
+
     const bars = chart.selectAll('.bar')//select all elements with class bar
         .data(selectedData, data => data.id);//bind data to the elements
-    // Update existing bars
-    chart.selectAll('.bar').attr('height', data => CHART_HEIGHT - y(data.value))
-        .attr('x', data => x(data.region))
-        .attr('y', data => y(data.value));
 
-    //create new bars
-    // chart.selectAll('.bar')
-    //     .data(selectedData, data => data.id)
-    //     .enter()//get access for the missing elements
-    //     .append('rect')//append a rect element
-    //     .classed('bar', true)//add class bar to the element
-    //     .attr('id', data => data.id)
-    //     .attr('width', x.bandwidth())//bandwidth gives us an equal space between the bars
-    //     .attr('height', data => CHART_HEIGHT - y(data.value))
-    //     .attr('x', data => x(data.region))
-    //     .attr('y', data => y(data.value));
+    // Add new bars
     bars.enter()
         .append('rect')//append a rect element
         .classed('bar', true)//add class bar to the element
-        .attr('id', data => data.id)
+        .attr('id', data => data.id);
+
+    // Update existing bars
+    chart.selectAll('.bar').attr('height', data => CHART_HEIGHT - y(data.value))
         .attr('width', x.bandwidth())//bandwidth gives us an equal space between the bars
         .attr('height', data => CHART_HEIGHT - y(data.value))
         .attr('x', data => x(data.region))
-        .attr('y', data => y(data.value))
-        .merge(bars)
-        .transition()
-        .duration(500)
-        .attr('width', x.bandwidth());
+        .attr('y', data => y(data.value));
 
 
     // Remove redundant bars
@@ -88,6 +89,7 @@ function renderChart(){
         .attr('y', data => y(data.value) - 20)
         .text(data => data.value)
         .attr('x', data => x(data.region) + x.bandwidth() / 2);
+        // .attr('x', data => x(data.region)+5);
     //Remove the labels that are not needed anymore
     chart.selectAll('.label').data(selectedData, data => data.id).exit().remove();
 
@@ -97,14 +99,17 @@ function renderChart(){
         toggleSelected(data);
         listItems.select(`#${data.target.id}`).property('checked', false);
     });
+
+    //change the color of the first element
+    chart.selectAll('.bar').attr('fill', (data, index) => index === 0 ? 'red' : 'steelblue');
 }
 
 
 renderChart();
 
-//add and remove elements
 
-//keep track of unchecked regions
+/* ******************************************* */
+/* ************** checkboxes ********************* */
 let unselectedIds = [];
 
 const listItems = d3.select('#data')
@@ -114,6 +119,10 @@ const listItems = d3.select('#data')
     .enter()
     .append('li')
 listItems.append('span').text(data => data.region);
+
+
+/* ******************************************* */
+/* ************** CLICK LISTENERS ********************* */
 
 listItems.append('input')
     .attr('id', data => data.id)
@@ -127,40 +136,39 @@ listItems.append('input')
 function toggleSelected(data) {
     console.log(data.target.id)
     if (unselectedIds.indexOf(data.target.id) === -1) {
-        //if the id is not in the array
+        //if the id is not in the array, add it
         unselectedIds.push(data.target.id);
     } else {
+        //else remove it
         unselectedIds = unselectedIds.filter(id => id !== data.target.id);
         //only keep the ids that are not equal to the id of the data
     }
     console.log(data);
     console.log(unselectedIds);
-    selectedData = [
-        {id: 'd1', value: 10, region: 'USA'},
-        {id: 'd2', value: 11, region: 'India'},
-        {id: 'd3', value: 12, region: 'China'},
-        {id: 'd4', value: 6, region: 'Germany'},
-    ].filter(data => unselectedIds.indexOf(data.id) === -1);
+    selectedData = selectedData.filter(data => unselectedIds.indexOf(data.id) === -1);
     renderChart();
 }
 
-
+/* ******************************************* */
+/* ************** BUTTONS ********************* */
 
 const BUTTON_NAMES = [
     {id: 'b1', name: 'Reset'},
-    {id: 'b2', name: 'Test'},
-    {id: 'b3', name: 'Sort'}
+    {id: 'b2', name: 'Random'},
+    {id: 'b3', name: 'Sort'},
+    {id: 'b4', name: 'Add'}
 ];
-//add a button to reset the chart
+
 const btn = d3.select('#reset').select('button').data(BUTTON_NAMES).enter().append('button');
 btn.text(data => data.name)
     .attr('id', data => data.id)
     .attr('title', data => data.id)
-    .attr('type', 'btn');
+    .attr('type', 'button');
 
 d3.select('#b1').on('click', () => {
+    selectedData[0].value = DUMMY_VALUE[0];
     console.log('reset');
-    selectedData= DUMMY_DATA;
+    selectedData= DUMMY_DATA.filter(()=>true);
     unselectedIds = [];
     renderChart();
     listItems.select('input').property('checked', true);
@@ -171,17 +179,33 @@ d3.select('#b1').on('click', () => {
 d3.select('#b2').on('click', () => {
     console.log('btn02');
     selectedData[0].value = Math.round(Math.random()*15);
-    console.log(DUMMY_DATA[0].value);
+    console.log(DUMMY_VALUE);
 
     renderChart();
 }
 );
 d3.select('#b3').on('click', () => {
     console.log('btn03');
-    console.log(toString(DUMMY_DATA));
-    selectedData.sort((a, b) => a.value - b.value);
-    console.log(toString(DUMMY_DATA));
-    renderChart();
+    console.log(toString(selectedData));
+    selectedData.sort((a, b) => -a.value + b.value);
+    console.log(toString(selectedData));
+    // Update the positions of the bars
+    chart.selectAll(".bar")
+        .data(selectedData, data => data.id)
+        .transition()
+        .duration(500)
+        .attr("x", data => x(data.region))
+        .attr("y", data => y(data.value))
+        .attr("height", data => CHART_HEIGHT - y(data.value));
+
+    // Update the positions of the labels
+    chart.selectAll(".label")
+        .data(selectedData, data => data.id)
+        .transition()
+        .duration(500)
+        .attr("x", data => x(data.region) + x.bandwidth() / 2)
+        .attr("y", data => y(data.value) - 20);
+
 });
 
 function toString(data) {
@@ -191,4 +215,12 @@ function toString(data) {
     }
     return result;
 }
+
+d3.select('#b4').on('click', () => {
+    console.log('btn04');
+    numOFBars++;
+    selectedData.push({id: `d${numOFBars}`, value: (Math.round(Math.random()*12)), region: `region${numOFBars}`});
+    console.log(selectedData);
+    renderChart();
+});
 
